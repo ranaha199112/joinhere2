a89543f9a5fcdef11fa819027d984cfe48259c1a
shovonroy173 <shovonroy2003@gmail.com>
2025-05-08 18:29:47 +0600

    mod: debug 1

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ export default function Home({ posterId, adminId }) {
   Cookies.set("posterId", posterId);
 
   return (
-    <div className="container pt-[35px] flex flex-col items-center overflow-x-hidden ">
+    <div className="container pt-[35px] flex flex-col items-center overflow-x-hidden bg-red-500">
       <div className="w-[75%] md:w-full">
         <Image src={"/images/megapersonals.png"} alt="megaeprsonals" priority />
       </div>
